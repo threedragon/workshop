8241b756446058f37d75aabe6d471f972676b946
threedragon <canonholy@gmail.com>
2011-12-01 23:09:55 +0900

    変更に向けてのテスト追加

--- LruMap2/src/main/java/LruMap2/Lrumap.java
+++ LruMap2/src/main/java/LruMap2/Lrumap.java
@@ -5,46 +5,69 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-public class Lrumap {
+public class Lrumap<K,V> {
 
-	int sizeNum;
-	Map<String,String> map;
-	List<String> list;
+	private int sizeNum;
+	private Map<K,V> datamap;
+	private List<K> history;
 
-	Lrumap(int a)
+	public Lrumap(int in_size)
 	{
-		sizeNum = a;
-		map = new HashMap<String, String>();
-		list = new LinkedList<String>();
+		if (in_size < 1) {
+			throw new IllegalArgumentException();
+		}
+		sizeNum = in_size;
+		datamap = new HashMap<K, V>();
+		history = new LinkedList<K>();
 	}
-	int size()
+
+	int maxSize()
 	{
 		return sizeNum;
 	}
-	public void put(String string, String string2) {
-		// TODO 自動生成されたメソッド・スタブ
-		map.put(string , string2);
-		list.add(string);
 
-		if (map.size() > sizeNum)
-		{
-			map.remove(list.get(0));
-			list.remove(0);
+	public void put(K key, V value) {
+		if (history.indexOf(key) != -1) {
+			history.remove(key);
 		}
+		datamap.put(key, value);
+		history.add(key);
+
+		checkSize();
 	}
 
-	public String get(String string) {
-		// TODO 自動生成されたメソッド・スタブ
-		int num = list.indexOf(string);
+	public V get(K key) {
+		int num = history.indexOf(key);
 		if (num != -1)
 		{
-			list.remove(num);
-			list.add(string);
+			history.remove(num);
+			history.add(key);
 		}
-		return map.get(string);
-
+		return datamap.get(key);
 	}
 
+	public void ModifySize(int in_size) {
+
+		if (in_size <= 0)
+		{
+			throw new IllegalArgumentException();
+		}
+		sizeNum=in_size;
+		while(checkSize());
+	}
+
+	private boolean checkSize()
+	{
+		boolean overFlag = false;
+		if (history.size() > sizeNum)
+		{
+			datamap.remove(history.get(0));
+			history.remove(0);
+			overFlag = true;
+		}
+		return overFlag;
+
+	}
 
 
 }
--- LruMap2/src/test/java/LruMap2/AppTest.java
+++ LruMap2/src/test/java/LruMap2/AppTest.java
@@ -1,80 +1,60 @@
 package LruMap2;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest
-    extends TestCase
+
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-
-
+	@Test
     public void testLRUMap()
     {
-    	Lrumap lru = new Lrumap(3);
-    	assertEquals(3, lru.size());
-    	Lrumap lru1 = new Lrumap(4);
-    	assertEquals(4, lru1.size());
+    	Lrumap<String, String> lru = new Lrumap<String,String>(3);
+    	assertEquals(3, lru.maxSize());
+    	Lrumap<Object,Object> lru1 = new Lrumap<Object, Object>(4);
+    	assertEquals(4, lru1.maxSize());
     }
 
+	@Test
     public void testPutGet()
     {
-    	Lrumap lru= new Lrumap(2);
+    	Lrumap<String, String> lru= new Lrumap<String, String>(2);
     	lru.put("A","testA");
     	assertEquals("testA",lru.get("A"));
     	lru.put("B","testB");
     	assertEquals("testB",lru.get("B"));
     }
 
+	@Test
     public void testPut3Elements()
     {
-    	Lrumap lru = new Lrumap(1);
+    	Lrumap<String, String> lru = new Lrumap<String, String>(1);
     	lru.put("A", "testA");
     	lru.put("B", "testB");
     	assertNull(lru.get("A"));
 
-    	Lrumap lru2 = new Lrumap(1);
+    	Lrumap<String, String> lru2 = new Lrumap<String, String>(1);
     	lru2.put("B", "testB");
     	lru2.put("A", "testA");
     	assertNull(lru2.get("B"));
 
-    	Lrumap lru3 = new Lrumap(1);
+    	Lrumap<String, String> lru3 = new Lrumap<String, String>(1);
     	lru3.put("A", "testA");
     	lru3.put("B", "testB");
     	lru3.put("C", "testC");
     	assertNull(lru3.get("B"));
     }
 
+	@Test
     public void testTouch()
     {
-    	Lrumap lru = new Lrumap(2);
+    	Lrumap<String, String> lru = new Lrumap<String, String>(2);
     	lru.put("1","dragon");
     	lru.put("2", "liger");
     	lru.get("1");
@@ -82,10 +62,44 @@ public class AppTest
     	assertNull(lru.get("2"));
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testSize()
     {
-    	Lrumap lru = new Lrumap(0);
+    	new Lrumap<String, String>(0);
+    }
+
+    @Test
+    public void testDuplicate()
+    {
+    	Lrumap<String, String> lru = new Lrumap<String, String>(2);
+    	lru.put("1","dragon");
+    	lru.put("2", "liger");
+    	lru.put("1","dog");
+    	lru.put("3","giraff");
+
+    	assertEquals("dog",lru.get("1"));
+    }
+
+    @Test
+    public void testDynamicSize()
+    {
+    	Lrumap<String, String> lru = new Lrumap<String, String>(2);
+    	lru.put("A","iii");
+    	lru.put("B", "kkk");
+
+    	lru.ModifySize(1);
+    	assertNull(lru.get("A"));
+
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testDynamicSize1()
+    {
+    	Lrumap<String, String> lru = new Lrumap<String, String>(2);
+    	lru.put("A","iii");
+    	lru.put("B", "kkk");
+
+    	lru.ModifySize(-1);
     }
 
 }
